87d43d88438b8cfa0d5bee653ab9f9e4b58196c9
ikshitmadan <ikshitmadan71@gmail.com>
2023-09-06 17:59:24 +0530

    chamges

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const baseUrl=process.env.BASE_URL;
 console.log(baseUrl);
 
 app.use(cors({
-  origin:["https://chat-appfrontend-ji9b5agwh-ikshitmadan.vercel.app","https://chat-appfrontend-1fsz-jltjpepid-ikshitmadan.vercel.app","https://chat-appfrontend.vercel.app","http://localhost:5173","chat-appfrontend-dl46zxxsn-ikshitmadan.vercel.app"],
+  origin:["https://chat-appfrontend-ji9b5agwh-ikshitmadan.vercel.app","https://chat-appfrontend-1fsz-jltjpepid-ikshitmadan.vercel.app","https://chat-appfrontend.vercel.app","http://localhost:5173","https://chat-appfrontend-1fsz.vercel.app"],
   credentials:true,
   methods:["GET", "POST"]
 }));
